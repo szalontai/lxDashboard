73d6964f4fdd4bb45bb04fe6b0fd4dd997d83084
MARCELO SZALONTAI <mszalontai@gmail.com>
2024-02-06 20:22:13 -0300

    Update Vendas.py

--- Vendas.py
+++ Vendas.py
@@ -18,7 +18,7 @@ st.set_page_config(layout='wide')
 #params = st.query_params.to_dict()
 params = st.query_params.get_all('server')
 
-st.subheader("Server :"+params['server'])
+st.subheader("Server :"+params)
 
 print(params.keys()) 
 
